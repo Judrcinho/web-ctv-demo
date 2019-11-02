f52dbf70cfc4cd6944b33ff22335a202a87bc972
Judrcinho <evgeniya.yurieva@gmail.com>
2019-11-02 11:20:12 +1100

    Updates

---
+++ precache-manifest.0b63a92fbbfcbaf43da259a0a56ddd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97af466f2e5b27e5ec56efd105bcd6a9",
+    "url": "/web-ctv/index.html"
+  },
+  {
+    "revision": "0dbc8205a502e6c56d82",
+    "url": "/web-ctv/static/js/2.c5e302f6.chunk.js"
+  },
+  {
+    "revision": "de4a7d0ef0dab6f16865",
+    "url": "/web-ctv/static/js/main.56cd7202.chunk.js"
+  },
+  {
+    "revision": "f63d8a19c78eba2b91c3",
+    "url": "/web-ctv/static/js/runtime-main.23fc0e17.js"
+  },
+  {
+    "revision": "2a3f011a74b71cac77f0134976aad150",
+    "url": "/web-ctv/static/media/logo.2a3f011a.png"
+  },
+  {
+    "revision": "25b4e634dae07d5d77ebe67a3d0743b2",
+    "url": "/web-ctv/static/media/tile.25b4e634.jpg"
+  }
+]);
