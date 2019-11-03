8d77b1b32232b40b70209a9ae67633f2ab05bb01
Judrcinho <evgeniya.yurieva@gmail.com>
2019-11-03 20:04:52 +1100

    add test and gh-pages package

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Footer from './components/footer/footer';
 import App from './App';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+// globally defined container var
+let container:any  = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  ReactDOM.render(<App />, container);
+});
+
+afterEach(() => {
+  // clean up on exit
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  // empty
 });
+
+it('subcomponent footer text rendered - default', () => {
+  expect(container.textContent).toBe("Home and Away");
+});
+
+
+it('subcomponent footer text rendered - override variable', () => {
+  act(() => {
+    render(<Footer text="Channel 7" />, container);
+  });
+  expect(container.textContent).toBe("Channel 7");
+});
+
+
+
